a3d336260b3415fa1e8e130872a44ddb5cfb34cc
Abir Hossen <abir.cse4@gmail.com>
2025-01-01 17:24:21 +0600

    done

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -6,6 +6,9 @@ const ErrorPage = () => {
     return (
         <div>
               <Navbar></Navbar>
+              <div className='w-10/12 mx-auto'>
+                <img className='mx-auto' src="https://i.ibb.co.com/NrGjttC/404.gif" alt="" />
+              </div>
               <Footer></Footer> 
 
 
